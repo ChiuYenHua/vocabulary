b07abd798f5276ee7d390db5ff4b1534455156b9
csmkzava <csmkzava@gmail.com>
2023-06-07 01:31:26 +0800

    Access google sheet into pandas not SQL

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@
 import streamlit as st
 from google.oauth2 import service_account
 from gsheetsdb import connect
+import gspread
+import pandas as pd
 
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
@@ -11,19 +13,18 @@ credentials = service_account.Credentials.from_service_account_info(
         "https://www.googleapis.com/auth/spreadsheets",
     ],
 )
-conn = connect(credentials=credentials)
+
+gs = gspread.authorize(credentials)
 
 # Perform SQL query on the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_resource(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
+def get_googleSheet(sheet_url):
+    sheet = gs.open_by_url(sheet_url)
+    worksheet = sheet.get_worksheet(0)
+    return worksheet.get_all_records()
 
 sheet_url = st.secrets["private_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-# Print results.
-for row in rows:
-    st.write(f"{row}")
+df = pd.DataFrame(get_googleSheet(sheet_url))
+st.write(df)
