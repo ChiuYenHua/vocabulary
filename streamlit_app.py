3430f10b0bdc555f94e77fd1221920dc4e0f54d9
csmkzava <csmkzava@gmail.com>
2023-06-07 00:27:04 +0800

    first time access to google sheet done

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,29 @@
-# .streamlit/secrets.toml
+# streamlit_app.py
 
-public_gsheets_url = "https://docs.google.com/spreadsheets/d/1YJkKkj6uJs8BIqmFMfx-OmpL7ktPxa4Yx74B3g6gvow/edit?usp=sharing"
+import streamlit as st
+from google.oauth2 import service_account
+from gsheetsdb import connect
+
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_resource(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["private_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row}")
