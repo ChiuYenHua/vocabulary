77cba3e3984143c63fc22903ee66dd56178d74cf
csmkzava <csmkzava@gmail.com>
2023-06-08 01:54:41 +0800

    add new python file using_secret.py

--- pages/choice_exam/single_choice.py
+++ pages/choice_exam/single_choice.py
@@ -1,18 +1,23 @@
 import streamlit as st
 import streamlit_book as stb
 from streamlit_option_menu import option_menu
+from using_secret import get_googleSheet
+
+
+df_name_list, df_list = get_googleSheet()
 
 
 #------------------------------------- Single Choice Question -------------------------------------#
-selected = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'],
-                        icons=['house', 'cloud-upload', "list-task", 'gear'],
-                        key='menu_5', orientation="horizontal")
+selected = option_menu(None, df_name_list,
+                        icons=['lightbulb'] * len(df_name_list),
+                        key='menu', orientation="horizontal")
 
-if selected=='Home':
-    st.title("Single Choice Question")
-    st.header("Question with minimal arguments")
+for index, name in enumerate(df_name_list):
+    if selected==name:
+        st.title("Single Choice Question")
+        st.header("Question with minimal arguments")
 
-    stb.single_choice("What does pandas (the library) stands for?",
-                        ["The cutest bear", "Panel Data", 
-                        "Pure Adamantium Numeric Datasets And Stuff", "PArties & DAtaSets"],
-                        1)
+        stb.single_choice("What does pandas (the library) stands for?",
+                            ["The cutest bear", "Panel Data", 
+                            "Pure Adamantium Numeric Datasets And Stuff", "PArties & DAtaSets"],
+                            1)
