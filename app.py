e093c191afafe789e4b5ef0e8675c3acf21e3a44
csmkzava <csmkzava@gmail.com>
2023-06-08 01:11:54 +0800

    service_account nameeror

--- app.py
+++ app.py
@@ -20,49 +20,49 @@ def main():
         matching_files=[]
 
     # Show the single page or the whole app
-    if len(matching_files)>0:
-        view_file = sorted(matching_files)[0]
-        stb.render_file(view_file)
-        # Add info on where to find the whole app
-        url = "https://stbook-methods.streamlitapp.com/"
-        html_link = f'[{url}]({url})'
-        mkd = f"See full app at {html_link}"
-        st.markdown(mkd)
-    else:
+    # if len(matching_files)>0:
+    #     view_file = sorted(matching_files)[0]
+    #     stb.render_file(view_file)
+    #     # Add info on where to find the whole app
+    #     url = "https://stbook-methods.streamlitapp.com/"
+    #     html_link = f'[{url}]({url})'
+    #     mkd = f"See full app at {html_link}"
+    #     st.markdown(mkd)
+    # else:
         # Streamit book properties
-        stb.set_book_config(menu_title="streamlit_book",
-                            menu_icon="lightbulb",
-                            options=[
-                                    "True/False Question",
-                                    "Single Choice Question",
-                                    "Multiple Choice Question",
-                                    "To Do List",
-                                    "Text Input",
-                                    "Code Input",
-                                    "Echo",
-                                    "Colored Expanders",
-                                    "Share",
-                                    "Floating Button",
-                                    "Answers", 
-                                    "Admin", 
-                                    ], 
-                            paths=[
-                                    current_path / "docs/true_or_false", 
-                                    current_path / "docs/single_choice",
-                                    current_path / "docs/multiple_choice",
-                                    current_path / "docs/to_do_list", 
-                                    current_path / "docs/text_input", 
-                                    current_path / "docs/code_input", 
-                                    current_path / "docs/goodies/00_echo.py", 
-                                    current_path / "docs/goodies/00_colored_expanders.py",
-                                    current_path / "docs/goodies/00_share.py",
-                                    current_path / "docs/goodies/00_button.py",
-                                    current_path / "docs/answers",
-                                    current_path / "docs/admin",
-                                    ],
-                            save_answers=save_answers,
-                            #display_page_info=False,
-                            )
+    stb.set_book_config(menu_title="streamlit_book",
+                        menu_icon="lightbulb",
+                        options=[
+                                "True/False Question",
+                                "Single Choice Question",
+                                "Multiple Choice Question",
+                                "To Do List",
+                                "Text Input",
+                                "Code Input",
+                                "Echo",
+                                "Colored Expanders",
+                                "Share",
+                                "Floating Button",
+                                "Answers", 
+                                "Admin", 
+                                ], 
+                        paths=[
+                                current_path / "docs/true_or_false", 
+                                current_path / "docs/single_choice",
+                                current_path / "docs/multiple_choice",
+                                current_path / "docs/to_do_list", 
+                                current_path / "docs/text_input", 
+                                current_path / "docs/code_input", 
+                                current_path / "docs/goodies/00_echo.py", 
+                                current_path / "docs/goodies/00_colored_expanders.py",
+                                current_path / "docs/goodies/00_share.py",
+                                current_path / "docs/goodies/00_button.py",
+                                current_path / "docs/answers",
+                                current_path / "docs/admin",
+                                ],
+                        save_answers=save_answers,
+                        display_page_info=False,
+                        )
 
 if __name__ == "__main__":
     main()
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,58 +1,26 @@
 import streamlit as st
-from google.oauth2 import service_account
-from gsheetsdb import connect
-import gspread
-import pandas as pd
-
-class Vocabulary:
-    def __init__(self):
-        self.df_name_list, self.df_list = self.getSheet()
-
-    def getSheet(self):
-        # Create credential from GCP service account
-        credentials = service_account.Credentials.from_service_account_info(
-            st.secrets["gcp_service_account"],
-            scopes=[
-                "https://www.googleapis.com/auth/spreadsheets",
-            ],
-        )
-
-        # Using credentials to access google sheet spread
-        gs = gspread.authorize(credentials)
-
-        # Uses st.cache_data to only rerun when the query changes or after 10 min.
-        @st.cache_resource(ttl=600)
-        def get_googleSheet(sheet_url):
-            # Make list to store all google worksheets into DataFrame and name, too
-            df_list = []
-            df_name_list = []
-            # Get sheet object
-            sheet = gs.open_by_url(sheet_url)
-            
-            # Loop through 'all worksheets' in google sheet
-            for i in range(len(sheet.worksheets())):
-                every_worksheet = sheet.get_worksheet(i)
-
-                df_list.append(pd.DataFrame(every_worksheet.get_all_records()))
-                df_name_list.append(every_worksheet.title)
-            
-            return df_name_list, df_list
-
-        # Url of private google sheet
-        sheet_url = st.secrets["private_gsheets_url"]
-
-        return get_googleSheet(sheet_url)
+import streamlit_book as stb
+from pathlib import Path
     
-    def main(self):
-        st.write(self.df_name_list)
-        st.write(self.df_list)
 
+    
+def main():
+    save_answers = False
+    current_path = Path(__file__).parent.absolute()
 
-
-
+    stb.set_book_config(menu_title="Cool Stuff",
+                    menu_icon="yin-yang",
+                    options=[
+                            "Single Choice Question",
+                            ], 
+                    paths=[
+                            current_path / "pages/choice_exam/single_choice.py",
+                            ],
+                    save_answers=save_answers,
+                    display_page_info=False,
+                    )
 
 
 
 if __name__ == "__main__":
-    voc = Vocabulary()
-    voc.main()
+    main()
