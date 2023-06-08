66c26d0304fee8881b9682c37f676dea3ec2bf0f
csmkzava <csmkzava@gmail.com>
2023-06-08 20:34:48 +0800

    fix requirments.txt

--- pages/choice_exam/single_choice.py
+++ pages/choice_exam/single_choice.py
@@ -2,22 +2,26 @@ import streamlit as st
 import streamlit_book as stb
 from streamlit_option_menu import option_menu
 from using_secret import get_googleSheet
-
-
-df_name_list, df_list = get_googleSheet()
+from pages.choice_exam.single_choice_function import question_builder
 
 
 #------------------------------------- Single Choice Question -------------------------------------#
+st.title("Single Choice Question")
+#st.header("Question with minimal arguments")
+
+# Get google sheet data from another python file
+df_name_list, df_list = get_googleSheet()
+
 selected = option_menu(None, df_name_list,
                         icons=['lightbulb'] * len(df_name_list),
                         key='menu', orientation="horizontal")
 
+
 for index, name in enumerate(df_name_list):
     if selected==name:
-        st.title("Single Choice Question")
-        st.header("Question with minimal arguments")
+        question_list, options_list, answer_list = question_builder(df_list[index])
 
-        stb.single_choice("What does pandas (the library) stands for?",
-                            ["The cutest bear", "Panel Data", 
-                            "Pure Adamantium Numeric Datasets And Stuff", "PArties & DAtaSets"],
-                            1)
+        for i in range(len(answer_list)):
+            stb.single_choice(question_list[i],
+                                options_list[i],
+                                answer_list[i])
---
+++ pages/choice_exam/single_choice_function.py
@@ -0,0 +1,74 @@
+import os
+import pandas as pd
+import random
+import streamlit as st
+
+#------------------------------------- Build questions in shuffle weighted way -------------------------------------#
+def random_weighted_select(english_list, weight, count):
+    question_keys = []
+    keys = english_list.copy()
+
+    while(len(question_keys)<count):
+        temp_question = random.choices(keys, weights=weight, k=1)[0]
+        if temp_question not in question_keys:
+            question_keys.append(temp_question)
+
+            index = keys.index(temp_question)
+            del keys[index]
+            del weight[index]
+
+    return question_keys
+
+def question_builder(df):
+    #------------------------------------ Preprocess Data ------------------------------------#
+    # Read data of right_wrong 
+    if os.path.isfile('quiz_statistics.csv'):
+        df_weight = pd.read_csv('quiz_statistics.csv')
+        # Check if vocabulary increase or decrease
+        # df_weight = vocabulary_increase_decrease(df, df_weight)
+    else:
+        df_weight = df.copy()
+        df_weight['weight'] = 1000
+
+    # list of every column in DataFrame
+    weight_list = df_weight['weight'].to_list()
+    english_list = df_weight['english'].to_list()
+    chinese_list = df_weight['chinese'].to_list()
+
+    # Get shuffle vocabulary keys for 10 quesiton
+    selected_english = random_weighted_select(english_list , weight_list, 10)  
+
+
+    #------------------------------------ Build question ------------------------------------#
+    question_list = []
+    for index, key in enumerate(selected_english):
+        question_list.append(f'Q{index+1}. {key}')
+
+    #------------------------------------ Build options + answer ------------------------------------#
+    options_list = []
+    answer_list = []
+
+    shuffled_chinese_list = chinese_list.copy()
+
+    for every_selected_english in selected_english:
+        # selected chinese in this question
+        every_selected_chinese = chinese_list[english_list.index(every_selected_english)]
+
+        # Make 4 options of each question (4 options can't include answer)
+        while(True):
+            random.shuffle(shuffled_chinese_list)
+            shuffle_4_options = shuffled_chinese_list[:4]
+
+            if every_selected_chinese not in shuffle_4_options:
+                break
+
+        # Substituted 4 options with 1 answer in it
+        answer_position = random.randint(0,3)
+        shuffle_4_options[answer_position] = every_selected_chinese
+
+        # Append options and answer
+        options_list.append(shuffle_4_options)
+        answer_list.append(answer_position)
+
+
+    return question_list, options_list, answer_list
